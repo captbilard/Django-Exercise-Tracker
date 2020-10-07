ac8b36157bd8c0b5fe7a50d2b4e21781492f7c25
captbilard <bilardosinulu@gmail.com>
2020-10-07 21:46:33 +0100

    Updated to fit flake8 standard

--- exercise_tracker/api/tests.py
+++ exercise_tracker/api/tests.py
@@ -35,37 +35,30 @@ class EndpointTestCase(APITestCase):
     def setUp(self):
         url = reverse('users-list')
         self.user = self.client.post(url, data={
-            "username":"Tester1"
+            "username": "Tester1"
         })
         self.response = self.client.get('/api/users/1/')
 
     def test_create_user(self):
-        data = {"username": "Tester2"}
+        data = {"username": "Tester4"}
         response = self.client.post('/api/users/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        #self.assertEqual(Users.objects.get().username, "Tester2")
-    
+
     def test_get_all_users(self):
         response = self.client.get('/api/users/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-    
+
     def test_add_exercise(self):
         url = reverse('exercise-list')
         data = {
             "description": "Sit ups",
-            "duration":"30",
+            "duration": "30",
             "username": self.response.data['url']
         }
         response = self.client.post(url, data, format='json')
-        #print(self.response.data, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['date'], "2020-10-06")
-    
+        self.assertEqual(response.data['date'], "2020-10-07")
+
     def test_get_exercise_log_user(self):
-        #user_id = self.response.data['id']
         response = self.client.get('/api/exercise/log/1/')
-        for item in response:
-            print(item)
-        print(response)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
